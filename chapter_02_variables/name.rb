5c6ac85d3c1c714b13ed1a47fbc4f33bb0a78ae9
Femke Knottenbelt <femknotgithub@gmail.com>
2018-01-14 14:23:57 +0000

    modified name.rb for exercise 4 in chapter 2

--- chapter_02_variables/name.rb
+++ chapter_02_variables/name.rb
@@ -1,6 +1,8 @@
 # name.rb
 
-print "What is your name? "
-name = gets.chomp
+puts "What is your first name? "
+first_name = gets.chomp.capitalize
+puts "What is your last name? "
+last_name = gets.chomp.capitalize
 
-puts "Welcome #{name}!"
+puts "Welcome #{first_name} #{last_name}!"
