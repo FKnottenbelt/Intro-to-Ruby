5712eed5856bb273b698c1512bc9a34e48caab99
Femke Knottenbelt <femknotgithub@gmail.com>
2018-01-22 11:00:07 +0000

    add chapter 10 exercise 16

---
+++ chapter_10_exercises/exercise_16.rb
@@ -0,0 +1,6 @@
+# exercise 16
+
+p a = ['white snow', 'winter wonderland', 'melting ice',
+     'slippery sidewalk', 'salted roads', 'white trees']
+
+p arr_new = a.map { |item|  item.split(" ")}.flatten
